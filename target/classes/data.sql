88dfdca33acf47a7aea3a2fff4c2386fd8dfaea5
MinerNik <sna9567@gmail.com>
2022-10-22 16:38:23 +0300

    Fix persistable model.User up Base H2

---
+++ target/classes/data.sql
@@ -0,0 +1,4 @@
+//INSERT INTO USERS (EMAIL, FIRST_NAME, LAST_NAME, PASSWORD)
+    //("Sychev", "Nikita", "Antonovich", "1987-07-21","user@gmail.com","89128529567","1234567890"))
+  //   VALUES ('user@gmail.com', 'User_First', 'User_Last', 'password'),
+    //             ('admin@javaops.ru', 'Admin_First', 'Admin_Last', 'admin');
